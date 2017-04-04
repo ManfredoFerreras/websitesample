0a71af3d47b418744bb43b22a1a0f18c6484e00f
manfredoferreras@gmail.com <manfredoferreras@gmail.com>
2017-04-04 10:20:12 -0400

    oiok

---
+++ WebSite/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using WebSite.Models;
+using EPS.BO.ADO;
+
+namespace WebSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        IAuthenticationManager Authentication
+        {
+            get { return HttpContext.GetOwinContext().Authentication; }
+
+
+        }
+
+        // GET: Account
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View(new LoginViewModel());
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            ClienesADO oAdo = new ClienesADO();
+
+            var users = oAdo.ReturnByEPS(model.UserName);
+
+
+            if (users.CTE_CODIGO_VOICE == model.Password)
+            {
+                string sName  = users.CTE_NOMBRE.TrimEnd() +" " + users.CTE_APELLIDO.ToString() + "(" + users.CTE_NUMERO_EPS.TrimEnd() +")";
+
+                var identity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, sName), }, DefaultAuthenticationTypes.ApplicationCookie);
+
+                //identity.Name = users.CTE_NUMERO_EPS.TrimEnd() + "-" + users.CTE_NUMERO_EPS.TrimEnd() + users.CTE_APELLIDO.ToString();
+
+
+
+                Authentication.SignIn(new AuthenticationProperties
+                {
+                    IsPersistent = false
+                }, identity);
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
+            }
+        }
+
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            Authentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebSite/Controllers/NavbarController.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSite.BO.CC;
+using WebSite.BO.Models;
+
+namespace WebSite.Controllers
+{
+  
+    public class NavbarController : Controller
+    {
+
+        public ActionResult Navbar()
+        {
+
+            MenuHandle oMenu = new MenuHandle();
+
+            var items = oMenu.MenuSuperior();
+
+            return PartialView("_Navbar", items);
+        }
+    }
+}
